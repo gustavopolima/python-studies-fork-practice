20ec97ea5c7bb2fe66842b5f88b1f12c3c6bfb80
gustavopolima <167944171+gustavopolima@users.noreply.github.com>
2024-04-24 15:29:08 +0000

    ok

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,10 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+# Criando uma variável que armazena uma string
+minha_string = "Olá, mundo!"
+
+# Criando uma lista que armazena várias strings
+minha_lista = ["Python", "é", "uma", "linguagem", "de", "programação"]
+
+# Imprimindo a variável e a lista
+print("String armazenada:", minha_string)
+print("Lista de strings armazenadas:", minha_lista)
